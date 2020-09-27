07a75a998e3dac76b7f98f890d6259ee3df03ccc
Axel Galicia <agalicia@architech.ca>
2020-09-27 03:05:17 -0400

    add url 80

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 //Socket.IO
-const socket = io.connect('http://juegalareta.com:4000');
+const socket = io.connect('http://juegalareta.com');
 
 const field = document.getElementById('field');
 const ball = document.getElementById('ball');
